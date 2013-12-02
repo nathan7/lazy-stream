f216637ce994473c7a24830d0886ba93f4f348ec
Nathan Zadoks <nathan@nathan7.eu>
2013-12-02 08:44:00 +0100

    rewrite and support for Writable/Duplex

--- README.md
+++ README.md
@@ -7,8 +7,12 @@
     npm install lazy-stream
 
 ## API
-### LazyStream(opts, Factory)
-### LazyStream(Factory)
+### lazyStream.Readable(opts, Factory)
+### lazyStream.Readable(Factory)
+### lazyStream.Writable(opts, Factory)
+### lazyStream.Writable(Factory)
+### lazyStream.Duplex(opts, Factory)
+### lazyStream.Duplex(Factory)
 
   Returns a lazy stream.
   It will call `new Factory(opts)` when anyone tries to read from it, and pass data through from the stream returned.
--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 'use strict';
 var ReadableStream = require('stream').Readable
+  , WritableStream = require('stream').Writable
+  , DuplexStream = require('stream').Duplex
   , inherits = require('utils').inherit
 
-inherits(LazyStream, ReadableStream)
 function LazyStream(opts, factory) {
-  if (!(this instanceof LazyStream))
-    return new LazyStream(opts, factory)
   if (typeof opts === 'function') {
     factory = opts
     opts = {}
   }
-  ReadableStream.call(this, opts)
   this._factory = factory
   this._stream = null
   this._opts = opts
 }
 
-LazyStream.prototype._read = function(size) {
-  var self = this
-    , stream = this._stream
+function think(lazy) {
+  var stream = lazy._stream
   if (stream === null) {
-    stream = this._stream = new this._factory(this._opts)
-    this._factory = null
-    this._opts = null
+    stream = lazy._stream = new lazy._factory(lazy._opts)
+    lazy._factory = null
+    lazy._opts = null
   }
-  stream.once('readable', function() {
-    self.push(stream.read(size))
+  return lazy
+}
+
+exports.Readable = LazyReadableStream
+inherits(LazyReadableStream, ReadableStream)
+function LazyReadableStream(opts, factory) {
+  if (!(this instanceof LazyReadableStream))
+    return new LazyReadableStream(opts, factory)
+  ReadableStream.call(this, opts)
+  LazyStream.call(opts, factory)
+}
+
+LazyReadableStream.prototype._read = function(size) { var self = this
+  think(this).once('readable', function() {
+    self.push(this.read(size))
   })
 }
+
+exports.Writable = LazyWritableStream
+inherits(LazyWritableStream, WritableStream)
+function LazyWritableStream(opts, factory) {
+  if (!(this instanceof LazyWritableStream))
+    return new LazyWritableStream(opts, factory)
+  WritableStream.call(this, opts)
+  LazyStream.call(opts, factory)
+}
+
+LazyWritableStream.prototype._write = function(chunk, encoding, callback) {
+  think(this).write(chunk, encoding, callback)
+}
+
+exports.Duplex = LazyDuplexStream
+inherits(LazyDuplexStream, DuplexStream)
+function LazyDuplexStream(opts, factory) {
+  if (!(this instanceof LazyDuplexStream))
+    return new LazyDuplexStream(opts, factory)
+  DuplexStream.call(this, opts)
+  LazyStream.call(opts, factory)
+}
+
+LazyDuplexStream.prototype._read = LazyReadableStream.prototype._read
+LazyDuplexStream.prototype._write = LazyWritableStream.prototype._write
